45d1cea5da4e7db67eb12147046e1d2a9f6a2942
summer-o3o <ho_ho95@naver.com>
2023-11-04 10:00:36 +0900

    [Done #COMMON] 리액트, 리덕스, 라우팅, 린트, 프리티어 등 초기셋팅

---
+++ src/pages/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <>
+      <form>
+        <input value="" />
+        <button type="submit">전송</button>
+      </form>
+      <div>할일 목록</div>
+      <ul>
+        <li>dd</li>
+        <button type="button">삭제</button>
+      </ul>
+    </>
+  );
+};
+
+export default Main;
